144590d54cf55e4615f7affca4817f7203ff2001
Victor S. Ivanov <v.ivanov@forwardmg.ru>
2017-03-23 10:24:02 +0300

    strip comments, fix helper

--- src/com_extension_saperu/admin/controllers/settings.php
+++ src/com_extension_saperu/admin/controllers/settings.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  com_helloworld
- *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-/**
- * HelloWorlds Controller
- *
- * @since  0.0.1
- */
 class SaperuControllerSettings extends JControllerForm
 {
     protected function postSaveHook(JModelLegacy $model, $validData = array())
--- src/com_extension_saperu/admin/models/settings.php
+++ src/com_extension_saperu/admin/models/settings.php
@@ -1,20 +1,5 @@
 <?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  com_helloworld
- *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-
-/**
- * HelloWorld Model
- *
- * @since  0.0.1
- */
 class SaperuModelSettings extends JModelAdmin
 {
     /**
